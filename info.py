ba5a4ba42a3e17df6b0dcc01f3f31067afd8cdd0
Matthew Bressler <148478050+matthewbressler@users.noreply.github.com>
2023-12-17 17:01:58 -0600

    Add files via upload

---
+++ info.py
@@ -0,0 +1,76 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "images/GT_Headshot.JPG"
+about_me = "My name is Matthew Bressler and I am a Computer Science major at Georgia Tech."
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_img_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_img_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_img_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/matthew-bressler-79597b293/"
+my_github_url = "https://github.com/matthewbressler"
+my_email_address = "mbressler6@gatech.edu"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': '2027',
+    'GPA': 'N/A'
+}
+course_data = {
+    "code":["CS 1301", "CS 3001", "MATH 1554", "ENGL 1101"], 
+    "names":["Intro to Computing", "Computing, Society and Professionalism", "Linear Algebra", "English Composition I"], 
+    "semester_taken":["1st", "1st", "1st", "1st"],
+    "skills":["Learning the basics of Python", "CS ethics requirement", "Vectors, matrices and linear transformations", "English requirement"],
+    }
+experience_data = {
+    "Georgia Tech Baseball Analyst/Student Manager": (["- Tag Trackman during games and scrimmages to allow for post game reports",
+                                                                          "- Assist in projects involving Trackman, Hawk-Eye and Motion Capture", "- Assist with on-field and off-field activities including batting practice, scrimmages, laundry and equipment"],"images/GT_Headshot.JPG"),
+    "Baseball Development/Analytics Coach at MVP Sports Academy":(["- Assisted with 20+ high school baseball players’ development by collaborating with professional coaches",
+                                                           "- Analyzed pitching data (HB, IVB, tilt) and video through Rapsodo to improve specific pitches and mechanics of pitchers", "- Applied knowledge of physics principles (torque, projectile motion, air resistance) to manipulating ball flight"],"images/velo.jpg"),
+    "Stevenson Varsity Baseball Team Captain":(["- Consulted with coaches regarding creating and executing practice plans, scheduling team events, and establishing a team culture", "- Mentored 60+ players in the program including underclassmen", "- Led team to a top 8 finish in the IHSA playoffs"],"images/UnionCity2B.JPG")
+
+}
+
+projects_data = {
+    "Lab 1: Building a Website": "Created a person website",
+    "Lab 2: Building Interactive Websites": "Created a Buzzfeed themed quiz",
+    "Lab 3: Building Web Apps": "Created a Pokemon web app using Streamlit. Allows the user to input the name of a Pokemon to get information about it"
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 20,
+    "C": 10,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"Mandarin": "",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Mandarin": "Fluent",
+}
+leadership_data = {
+    "Advisory Leader for FMP": (["- Helped freshmen become acclimated with high school", "- Fostered relationships within the classroom"],"images/FMP.jpg"),
+
+}
+activity_data={
+    "Data Science @GT": ["- Working on completing data science bootcamp"]
+}
---
+++ portfolio.py
@@ -0,0 +1,110 @@
+import streamlit as st
+import info
+import pandas as pd
+
+#About Me
+def aboutMeSection():
+    st.header("About Me")
+    st.image(info.profile_picture, width = 500)
+    st.write(info.about_me)
+    st.write("---")
+aboutMeSection()
+
+#Sidebar Links
+def linksSection():
+    st.sidebar.header("Links")
+    st.sidebar.text("Connect with me on LinkedIn!")
+    linkedin_link = f'<a href="{info.my_linkedin_url}"><img src="{info.linkedin_img_url}" alt="LinkedIn" width="75" height="75"></a>'
+    st.sidebar.markdown(linkedin_link, unsafe_allow_html=True)
+
+    st.sidebar.text("Checkout my work!")
+    github_link = f'<a href="{info.my_github_url}"><img src="{info.github_img_url}" alt="GitHub" width="75" height="75"></a>'
+    st.sidebar.markdown(github_link, unsafe_allow_html=True)
+    
+    st.sidebar.text("Contact me!")
+    email_html = f'<a href="mailto:{info.my_email_address}"><img src="{info.email_img_url}" alt="Email" width="75" height="75"></a>'
+    st.sidebar.markdown(email_html, unsafe_allow_html=True)
+linksSection()
+
+#Education
+def educationSection(education_data, course_data):
+    st.header("Education")
+    st.subheader(f"**{education_data['Institution']}**")
+    st.write(f"**Degree:** {education_data['Degree']}")
+    st.write(f"**Graduation Date:** {education_data['Graduation Date']}")
+    st.write(f"**GPA:** {education_data['GPA']}")
+
+    st.write("**Relevant Coursework:** ")
+    coursework = pd.DataFrame(course_data)
+    st.dataframe(coursework, column_config = {
+        "code": "Course Code",
+        "names": "Course Names",
+        "semester_taken": "Semester Taken",
+        "skills": "Description"},
+        hide_index = True,
+        )
+    st.write("---")
+
+educationSection(info.education_data,info.course_data)
+
+#Professional Experience
+
+def experienceSection(experience_data):
+    st.header("Professional Experience")
+    for job_title, (job_description, image) in experience_data.items():
+        expander = st.expander(f"{job_title}")
+        expander.image(image, width = 250)
+        for bullet in job_description:
+            expander.write(bullet)
+    st.write("---")
+experienceSection(info.experience_data)
+
+#Projects
+
+def projectSection(projects_data):
+    st.header("Projects")
+    for project_name, project_description in projects_data.items():
+        expander = st.expander(f"{project_name}")
+        expander.write(project_description)
+    st.write("---")
+projectSection(info.projects_data)
+
+#Skills
+
+def skillsSection(programming_data, spoken_data):
+    st.header("Skills")
+    st.subheader("Programming Languages")
+    for skill, percentage in programming_data.items():
+        st.write(f"{skill}{info.programming_icons.get(skill)}")
+        st.progress(percentage)
+    st.header("Spoken Languages")
+    for spoken, proficiency in spoken_data.items():
+        st.write("{}{}: {}".format(spoken, info.spoken_icons.get(spoken), proficiency))
+    st.write("---")
+skillsSection(info.programming_data, info.spoken_data)
+
+#Activities
+
+def activitiesSection(leadership_data, activity_data):
+    st.header("Activities")
+    tab1, tab2 = st.tabs(["Leadership", "Extracurriculars"])
+    with tab1:
+        st.subheader("Leadership")
+        for title, (details, image) in leadership_data.items():
+            expander = st.expander("{}".format(title))
+            expander.image(image, width = 250)
+            for bullet in details:
+                expander.write(bullet)
+    with tab2:
+        st.subheader("Extracurriculars")
+        for title, details in activity_data.items():
+            expander = st.expander("{}".format(title))
+            for bullet in details:
+                expander.write(bullet)
+
+    st.write("---")
+                
+activitiesSection(info.leadership_data, info.activity_data)
+
+
+
