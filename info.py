752488f930aa4c60c4a9ad47eb761357f70ff837
Matthew Bressler <148478050+matthewbressler@users.noreply.github.com>
2024-10-03 11:46:23 -0400

    Update info.py

--- info.py
+++ info.py
@@ -31,10 +31,6 @@ experience_data = {
     "Georgia Tech Baseball Analyst": (["- Optimized defensive positioning by analyzing data on TruMedia, creating detailed positioning cards to enhance game strategy",
                                        "- Develop weekly hitting reports and work with 20+ hitters using HitTrax data to improve performance", 
                                        "-  Manage in-game data tagging and technology operations via Trackman, producing post-game reports for coaches"], "images/GT_Headshot.JPG"),
-    "Objects and Design Developer": (["- Collaborate with a cross-functional team to design and develop a restaurant search website for Atlanta",
-                                      "- Develop UI and UX using Django for back end functionality and Figma for design", 
-                                      "- Utilize object oriented programming principles to optimize code structure and project efficiently",
-                                      "- Follow user stories and apply the SCRUM framework to manage project workflow and conduct sprint reviews"],"images/oop.jpg"),
     "MVP Sports Baseball Development and Analytics Coach": (["- Assisted in the development of 20+ high school baseball players by collaborating with professional coaches", 
                                                              "- Analyzed pitching data (HB, IVB, tilt) and video using Rapsodo technology, resulting in improved mechanics and pitch specific adjustments", 
                                                              "- Applied physics principles including torque, projectile motion, and air resistance) to optimize ball flight"],"images/Velo.jpg")
