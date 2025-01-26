9f5d15014af329ee5e21a183fe64e73b574c1ebf
Matthew Bressler <148478050+matthewbressler@users.noreply.github.com>
2025-01-25 19:46:40 -0600

    Update info.py

--- info.py
+++ info.py
@@ -18,8 +18,8 @@ education_data ={
     'Degree': 'Bachelor of Science in Computer Science, Minor in Economics',
     'Institution': 'Georgia Institute of Technology',
     'Location': 'Atlanta, GA',
-    'Graduation Date': '2027',
-    'GPA': '4.0'
+    'Graduation Date': 'Fall 2026',
+    'GPA': '4.0 / 4.0'
 }
 course_data = {
     "code":["CS 1301", "CS 1332", "CS 3600", "ISYE 3770", "MATH 1554", "CS 2110", "CS 1331", "CS 2340"], 
@@ -27,22 +27,36 @@ course_data = {
     "skills":["Python", "Computer data structures and algorithms", "intelligent system design methodologies, search and problem solving, supervised and reinforced learning", "probability, probability distributions, point estimation, confidence intervals, hypothesis testing, linear regression", "eigenvalues, eigenvectors, applications to linear systems, least squares, diagonalization, quadratic forms", "basic computer hardware, machine language, assembly language, and C", "Object Oriented Programming in Java", "Object-oriented programming methods for dealing with large programs"],
     }
 experience_data = {
-    "Georgia Tech Baseball Analyst": (["- Optimized defensive positioning by analyzing data on TruMedia, creating detailed positioning cards to enhance game strategy",
-                                       "- Develop weekly hitting reports and work with 20+ hitters using HitTrax data to improve performance", 
-                                       "-  Manage in-game data tagging and technology operations via Trackman, producing post-game reports for coaches"], "images/GT_Headshot.JPG"),
-    "MVP Sports Baseball Development and Analytics Coach": (["- Assisted in the development of 20+ high school baseball players by collaborating with professional coaches", 
-                                                             "- Analyzed pitching data (HB, IVB, tilt) and video using Rapsodo technology, resulting in improved mechanics and pitch specific adjustments", 
-                                                             "- Applied physics principles including torque, projectile motion, and air resistance) to optimize ball flight"],"images/Velo.jpg")
+    "Georgia Tech Baseball Analyst": ([" - Develop a Stuff+ model for pitchers using Trackman data", 
+                                       "- Analyze data on TruMedia to optimize defensive positioning and create positioning cards",
+                                       "- Create visualizations and models to help coaches better understand pitching and hitting statistics", 
+                                       "-  Manage in-game data tagging and technology operations via Trackman"], "images/GT_Headshot.JPG"),
+    "MVP Sports Baseball Development and Analytics Coach": (["- Analyze pitching data (HB, IVB, tilt) and video from 20+ high school baseball players using Rapsodo technology", 
+                                                             "- Teach players how to use tools (bands, weighted balls, Core Velo Belt) to improve arm care and throwing velocity", 
+                                                             "- Work with coaches to personalize training and identify areas of improvement for players based on analysis"],"images/Velo.jpg")
 }
 
 projects_data = {
-    "Atlanta Food Finder": (["Website Link: https://sites.google.com/view/atl-food-finder/home",
-                             "- Collaborate with a cross-functional team to design and develop a restaurant search website for Atlanta",
-                             "- Develop UI and UX using Django for back end functionality and Figma for design", 
-                             "- Utilize object oriented programming principles to optimize code structure and project efficiently",
-                             "- Follow user stories and apply the SCRUM framework to manage project workflow and conduct sprint reviews"]),
-    "CSAS Baseball Data Challenge": "Use new data on bat speed and swing length to analyze pitcher/batter interaction and create productive lineups"
+  "CSAS Baseball Data Challenge": [
+    "- Leading a team conducting analysis on bat speed and swing length data",
+    "- Developing visualizations using Matplotlib and Seaborn to identify patterns in pitcher-batter interaction"
+  ],
+  "MVP Sports Velocity Camp Dashboard": [
+    "- Creating web app using Flask to generate personalized reports for players’ pulldown (throwing) data"
+  ],
+  "Atlanta Food Finder": [
+    "Website Link: https://sites.google.com/view/atl-food-finder/home",
+    "- Designed and developed frontend functionality for a restaurant search website for Atlanta",
+    "- Developed UI and UX using Figma for designing wireframe and Django for integrating backend functionality",
+    "- Used voice of customer and SCRUM to manage project framework and organization"
+  ],
+  "Spotify Wrapped": [
+    "Website Link: https://sites.google.com/view/atl-food-finder/home",
+    "- Designed and developed frontend and fullstack features for a web app showcasing users’ listening habits on Spotify",
+    "- Ensured user stories were being satisfied and discussed progress throughout the project with stakeholders"
+  ]
 }
+
 
 programming_data = {
     "Python": 90,
@@ -66,7 +80,7 @@ spoken_data = {
 }
 leadership_data = {
     "Advisory Leader for Stevenson Freshman Mentor Program": (["- Guided 20+ freshmen through their transition to high school by fostering relationships and leading daily activities", "- Created weekly agendas and communicated schedule with fellow mentors and the faculty advisor", "- Planned activities and presentations by collaborating with fellow mentors to promote a sense of community", "- Attended bi-weekly leadership lunches to improve leadership skills and discuss ways for improvement"],"images/FMP.jpg"),
-    "Stevenson Varsity Baseball Captain": (["- Collaborated with coaches to create and execute practice plans, organize team events, and establish a team culture", "- Mentored 60+ players in the program, guiding underclassmen and fostering leadership within the program", "- Led team to a top 8 finish in the IHSA playoffs through leadership and on-field performance"],"images/UnionCity2B.JPG")
+    "Stevenson Varsity Baseball Captain": (["- Collaborated with coaches to create and execute practice plans, organize team events, and promote a team culture", "- Mentored 60+ players in the program, guiding underclassmen and fostering leadership within the program", "- Led team to a top 8 finish in the IHSA playoffs through leadership and on-field performance"],"images/UnionCity2B.JPG")
 
 }
 activity_data={
